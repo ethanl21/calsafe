d0c33b3041bd07b24b95a609fb78acdd0ab19c6b
Henry Dinh <44953257+Cloudymizu@users.noreply.github.com>
2024-10-15 00:00:23 -0700

    Map and API update
    - Map now displays markers (albeit broken transparent markers)
    
    - Map currently only displays results for Orange County in the month of December 2021
    
    - Adjusted API to incorporate other views

--- api/urls.py
+++ api/urls.py
@@ -1,8 +1,18 @@
 # api/urls.py
 
-from django.urls import path
-from . import views
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
+from .views import LocationViewSet, SeverityViewSet, EnvironmentViewSet, AccidentViewSet, PartyViewSet, VictimViewSet, accident_list
+
+router = DefaultRouter()
+router.register(r'locations', LocationViewSet)
+router.register(r'severities', SeverityViewSet)
+router.register(r'environments', EnvironmentViewSet)
+router.register(r'accidents', AccidentViewSet)
+router.register(r'parties', PartyViewSet)
+router.register(r'victims', VictimViewSet)
 
 urlpatterns = [
-    path('accidents/', views.accident_list, name='accident-list'),
+    path('accidents/', accident_list, name='accident-list'),
+    path('', include(router.urls)),
 ]
--- api/views.py
+++ api/views.py
@@ -1,7 +1,9 @@
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
-from .models import Accident, Victim
-from .serializers import AccidentSerializer
+from rest_framework import viewsets
+from .models import Location, Severity, Environment, Accident, Party, Victim
+from .serializers import (LocationSerializer, SeveritySerializer, EnvironmentSerializer, 
+                          AccidentSerializer, PartySerializer, VictimSerializer)
 from django.db.models import Q
 
 @api_view(['GET'])
@@ -12,6 +14,7 @@ def accident_list(request):
     start_date = request.GET.get('start_date')
     end_date = request.GET.get('end_date')
     county = request.GET.get('county')
+    city = request.GET.get('city')
     crash_types = request.GET.get('crash_types')
     victim_filters = request.GET.get('victim_filters')
 
@@ -26,6 +29,10 @@ def accident_list(request):
     # Apply county filter
     if county:
         queryset = queryset.filter(location__county__icontains=county)
+
+    # Apply city filter
+    if city:
+        queryset = queryset.filter(location__city__icontains=city)
 
     # Apply crash types filter
     if crash_types:
@@ -42,3 +49,27 @@ def accident_list(request):
     # Serialize the data
     serializer = AccidentSerializer(queryset, many=True)
     return Response(serializer.data)
+
+class LocationViewSet(viewsets.ModelViewSet):
+    queryset = Location.objects.all()
+    serializer_class = LocationSerializer
+
+class SeverityViewSet(viewsets.ModelViewSet):
+    queryset = Severity.objects.all()
+    serializer_class = SeveritySerializer
+
+class EnvironmentViewSet(viewsets.ModelViewSet):
+    queryset = Environment.objects.all()
+    serializer_class = EnvironmentSerializer
+
+class AccidentViewSet(viewsets.ModelViewSet):
+    queryset = Accident.objects.all()
+    serializer_class = AccidentSerializer
+
+class PartyViewSet(viewsets.ModelViewSet):
+    queryset = Party.objects.all()
+    serializer_class = PartySerializer
+
+class VictimViewSet(viewsets.ModelViewSet):
+    queryset = Victim.objects.all()
+    serializer_class = VictimSerializer
